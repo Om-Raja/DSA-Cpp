1eef6f785c719d199d037a0b80e7397982ec310c
Om Raja <omraja451@gmail.com>
2023-12-28 03:50:56 +0530

    added more code to relational_operator.cpp

--- Lecture_02/relational_operator.cpp
+++ Lecture_02/relational_operator.cpp
@@ -6,5 +6,19 @@ int main()
     cout << (a > b) << endl;
     cout << (a == b) << endl;
     cout << (a < b) << endl;
+
+    bool first = (a == b);
+    bool second = (a != b);
+    bool third = (a >= b);
+    bool fourth = (a <= b);
+    bool fifth = (a > b);
+    bool sixth = (a < b);
+    
+    cout << "first is " << first << endl;
+    cout << "second is " << second << endl;
+    cout << "third is " << third << endl;
+    cout << "fourth is " << fourth << endl;
+    cout << "fifth is " << fifth << endl;
+    cout << "sixth is " << sixth << endl;
     return 0;
 }

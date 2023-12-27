d27aa1f4184fac0c2a96f7cc1a076c472570daa9
Om Raja <omraja451@gmail.com>
2023-12-28 03:22:55 +0530

    added unsingned.cpp & deleted typecasting.exe from local

---
+++ Lecture_02/unsigned_int.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    unsigned int a = 11234;
+    cout << a << endl;
+    unsigned int b = -11234;
+    cout << b << endl;
+    return 0;
+}

cee0bc444f985408f3a9cf6d4596911d4c39c95d
Om Raja <omraja451@gmail.com>
2023-12-28 02:09:40 +0530

    add typecasting code

---
+++ Lecture_02/typecasting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+    int a = 'b';
+    std::cout << a << std::endl;
+
+    double c = 'b';
+    std::cout << c << std::endl ;
+
+    float d = 'b';
+    std::cout << d << std::endl ;
+
+    char e = 97 ;
+    std::cout << e << std::endl;
+
+    char f = 654328 ;
+    std::cout << f << std::endl;
+}

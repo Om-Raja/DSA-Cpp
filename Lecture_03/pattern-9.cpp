da33180ce0a43f7016980fa4d405f9f644509786
Om Raja <omraja451@gmail.com>
2024-01-01 16:11:32 +0530

    added another way to solve pattern-9.cpp

--- Lecture_03/pattern-9.cpp
+++ Lecture_03/pattern-9.cpp
@@ -6,17 +6,38 @@ int main()
     cout << "Enter number of rows ";
     cin >> n;
 
+//     int row = 1;
+//     while (row <= n)
+//     {
+//         int col = 0;
+//         while (col < row)
+//         {
+//             cout << row + col;
+//             col++;
+//         }
+//         cout << endl;
+//         row++;
+//     }
+//     return 0;
+
+
+// another way :
+
     int row = 1;
+    
     while (row <= n)
     {
-        int col = 0;
-        while (col < row)
+        int value = row;
+        int column = 1;
+        while (column <= row)
         {
-            cout << row + col;
-            col++;
+            cout << value;
+            value++;
+            column++;
         }
         cout << endl;
         row++;
     }
-    return 0;
+
 }
+

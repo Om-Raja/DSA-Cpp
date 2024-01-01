fc7b5f8bcd85f94b0226ac7a7f0580f58008904c
Om Raja <omraja451@gmail.com>
2024-01-01 20:01:48 +0530

    add pattern-18.cpp

---
+++ Lecture_03/pattern-18.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number of rows ";
+    cin >> n;
+
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            cout << char('A' + n - i + j - 1) << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+    return 0;
+}

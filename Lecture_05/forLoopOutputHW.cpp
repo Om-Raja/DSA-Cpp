3f22f3e7724071584c1929d8d66b5d33d3e8f054
Om Raja <omraja451@gmail.com>
2024-01-02 12:30:08 +0530

    add forLoopOutputHW.cpp

---
+++ Lecture_05/forLoopOutputHW.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << i << " ";
+        i++;
+    }
+    cout << endl; 
+
+    // for (int i = 0; i <= 5; i--)
+    // {
+    //     cout << i << " ";
+    //     i++;
+    // }
+
+    for (int i = 0; i <= 15; i += 2)
+    {
+        cout << i << " ";
+
+        if (i&1)
+        {
+            continue;
+        }
+        i++;
+    }
+    cout << endl;
+
+    
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = i; j <= 5; j++)
+        {
+            cout << i << " " << j << endl;
+        }
+    }
+    return 0;
+}

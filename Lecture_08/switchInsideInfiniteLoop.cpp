968bb3bea91df0096ead81c315a8360f9d118ee9
Om Raja <omraja451@gmail.com>
2024-01-06 20:15:01 +0530

    modify switchInsideInfiniteLoop.cpp

---
+++ Lecture_08/switchInsideInfiniteLoop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    // use exit() to get out of infinite loop 
+    char ch = 'a';
+    while (1)
+    {
+        switch(ch)
+        {
+            case 'a' :
+            cout << "a ";
+            // break;
+            exit(0);
+
+            case 'b' :
+            cout << "b ";
+            // break;
+
+            default :
+            cout << "This is default";
+
+        }
+            // exit(0);
+    }
+    return 0;
+}
